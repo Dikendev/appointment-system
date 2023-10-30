178eacf1c61862bf9b69244b2e68f1c7180b4d44
Dikendev <diken.dev@gmail.com>
2023-10-29 23:55:40 -0300

    add user service

---
+++ src/domain/user/user.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { Prisma, PrismaClient, User } from '@prisma/client';
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly prisma: PrismaClient,
+  ) {}
+
+  async getUserCACHE(key: string): Promise<any> {
+    console.log('user getUserCACHE', key);
+
+    return await this.cacheManager.get(key);
+  }
+  async addToCACHE(key: string, value: any) {
+    return await this.cacheManager.set(key, value, 0);
+  }
+
+  async user(
+    UserWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: UserWhereUniqueInput,
+    });
+  }
+
+  async users(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithAggregationInput;
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+
+    return this.prisma.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+
+    return this.prisma.user.update({
+      where,
+      data,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
+}
