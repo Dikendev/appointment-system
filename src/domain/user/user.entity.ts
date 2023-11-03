65277699b2886a71813ae03101fbb063ce32853e
Dikendev <diken.dev@gmail.com>
2023-11-02 23:57:48 -0300

    add user DTO

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,9 +1,26 @@
 import { Booking } from '@prisma/client';
+import { Exclude, Expose } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class User {
   id: string;
   name: string;
   email: string;
   password: string;
+  bookings: Booking[];
+}
+
+export class CreateUserDTO {
+  name: string;
+  email: string;
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
+
+export class UserResponseDTO {
+  id: number;
+  name: string;
+  email: string;
   bookings: Booking[];
 }
