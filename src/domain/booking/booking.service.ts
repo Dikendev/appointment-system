bc0ef5d2f608d8dfe9f5b239e33fd6ab0e05cc0e
Dikendev <diken.dev@gmail.com>
2023-11-02 23:59:03 -0300

    add create booking service initial

--- src/domain/booking/booking.service.ts
+++ src/domain/booking/booking.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Booking, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class BookingService {}
+export class BookingService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createBooking(data: Prisma.BookingCreateInput): Promise<Booking> {
+    return this.prisma.booking.create({
+      data,
+    });
+  }
+}
