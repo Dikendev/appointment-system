3af7e27a59605c36c8ce1463fd3f20e8c604c8e0
Dikendev <diken.dev@gmail.com>
2023-11-16 23:34:59 -0300

    add migration prisma

---
+++ prisma/migrations/20231106202805_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "WorkingTime" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "time" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "WorkingTime_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Booking" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "clientId" INTEGER NOT NULL,
+    "serviceId" INTEGER NOT NULL,
+    "total" INTEGER NOT NULL,
+    "startAt" DATETIME NOT NULL,
+    "finishAt" DATETIME NOT NULL,
+    CONSTRAINT "Booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Booking_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Booking_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Booking" ("clientId", "finishAt", "id", "serviceId", "startAt", "total", "userId") SELECT "clientId", "finishAt", "id", "serviceId", "startAt", "total", "userId" FROM "Booking";
+DROP TABLE "Booking";
+ALTER TABLE "new_Booking" RENAME TO "Booking";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
