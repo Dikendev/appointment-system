c1fce7fc176efd6f18381258da7c620f92c00f54
Dikendev <diken.dev@gmail.com>
2024-02-12 23:43:10 -0300

    feat add booking service

--- src/domain/booking/booking.service.ts
+++ src/domain/booking/booking.service.ts
@@ -1,14 +1,68 @@
 import { Injectable } from '@nestjs/common';
-import { Booking, Prisma } from '@prisma/client';
-import { PrismaService } from 'src/prisma.service';
+import { Booking as BookingModel, Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class BookingService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaClient) {}
 
-  async createBooking(data: Prisma.BookingCreateInput): Promise<Booking> {
-    return this.prisma.booking.create({
+  async createBooking(data: Prisma.BookingCreateInput): Promise<BookingModel> {
+    console.log('data', data);
+    return await this.prisma.booking.create({
       data,
+    });
+  }
+
+  async getBookingByUser(id: string): Promise<BookingModel[]> {
+    return await this.prisma.booking.findMany({
+      where: {
+        userId: Number(id),
+      },
+      include: {
+        user: true,
+        client: true,
+        service: true,
+      },
+    });
+  }
+
+  async getAllBookings(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ServiceWhereUniqueInput;
+    where?: Prisma.ServiceWhereInput;
+    orderBy?: Prisma.ServiceOrderByWithRelationInput;
+  }): Promise<BookingModel[]> {
+    const { skip, take, orderBy } = params;
+
+    return await this.prisma.booking.findMany({
+      skip,
+      take,
+      orderBy,
+      include: {
+        client: true,
+        service: true,
+      },
+    });
+  }
+
+  async getBookingByDay(date: string): Promise<any[]> {
+    return this.prisma.booking.findMany({
+      where: {
+        AND: [
+          { startAt: { gte: new Date(date) } },
+          {
+            startAt: {
+              lt: new Date(
+                new Date(date).setDate(new Date(date).getDate() + 1),
+              ),
+            },
+          },
+        ],
+      },
+      include: {
+        user: true,
+        service: true,
+      },
     });
   }
 }
