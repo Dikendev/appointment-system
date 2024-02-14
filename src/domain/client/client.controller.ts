de15cec2c08ea23d2d6c51fdb8af625002f154c5
Dikendev <diken.dev@gmail.com>
2024-02-13 23:54:34 -0300

    fix: update route name and wrong async methods calls

--- src/domain/client/client.controller.ts
+++ src/domain/client/client.controller.ts
@@ -9,8 +9,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { ClientService } from './client.service';
-import { ClientDTO } from './client.model';
-import { Client as ClientModel } from '@prisma/client';
+import { ClientDTO, ClientResponseDTO } from './client.model';
 
 @Controller('client')
 export class ClientController {
@@ -18,7 +17,7 @@ export class ClientController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async createClient(@Body() data: ClientDTO): Promise<ClientModel> {
+  async createClient(@Body() data: ClientDTO): Promise<ClientResponseDTO> {
     const { name, email, phoneNumber, profilePicture } = data;
 
     return this.clientService.createClient({
@@ -31,20 +30,16 @@ export class ClientController {
 
   @Get('/:id/id')
   async getClient(@Param('id') id: string): Promise<ClientDTO> {
-    console.log('sssssss');
-
-    console.log('s', await this.clientService.client({ id: Number(id) }));
     return this.clientService.client({
       id: Number(id),
     });
   }
 
-  @Get('/get-all')
+  @Get('get-all')
   async getAllClients(): Promise<ClientDTO[]> {
-    console.log('1asas2');
-    const clients = this.clientService.clients({});
+    const clients = await this.clientService.clients({});
 
-    if (!(await clients).length) {
+    if (!clients.length) {
       throw new NotFoundException('No clients found');
     }
 
