a10865b47008d5b72f14406bba38b38c975896fe
Dikendev <diken.dev@gmail.com>
2024-02-13 23:48:21 -0300

    refactor: change get user method and fix methods bug

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -2,47 +2,79 @@ import {
   Body,
   Controller,
   Get,
-  HttpCode,
   Inject,
+  NotFoundException,
   Param,
   Post,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CacheDTO } from 'src/utils/mappers/user/cacheDTO';
-import { UserResponseDTO } from './user.entity';
+import { CreateUserDTO } from './model/user.model';
+import { User, User as UserModel } from '@prisma/client';
+import { UserResponseDto } from './model/user.response';
 
 @Controller('user')
 export class UserController {
   constructor(@Inject(UserService) private userService: UserService) {}
 
-  @Get(':key')
-  @HttpCode(200)
-  async getUserCACHE(@Param('key') key: string): Promise<string> {
-    console.log('key', key);
+  @Post()
+  async createUser(
+    @Body() createUserDTO: CreateUserDTO,
+  ): Promise<UserResponseDto> {
+    const user = await this.userService.createUser(createUserDTO);
 
-    const user = await this.userService.getUserCACHE(key);
-    console.log('user', user);
     return user;
   }
 
-  @Post()
-  async createUserCACHE(@Body() cacheDTO: CacheDTO) {
-    console.log('CacheDTO', cacheDTO);
+  async getUserCache(@Param('key') key: string): Promise<string> {
+    const user = await this.userService.getUserCache(key);
+    return user;
+  }
 
+  async createUserCache(@Body() cacheDTO: CacheDTO) {
     const { key, value } = cacheDTO;
-    console.log('key', key);
-    console.log('value', value);
 
-    return await this.userService.addToCACHE(key, value);
+    return await this.userService.addToCache(key, value);
   }
 
   @Get(':id/bookings')
-  async getUserWithBookings(@Param('id') id: number): Promise<UserResponseDTO> {
+  async getUserWithBookings(@Param('id') id: number): Promise<UserResponseDto> {
     return this.userService.findUserWithBookings(id);
   }
 
   @Get(':id/times')
   async getUserTime(@Param('id') id: number) {
     return this.userService.getUserWorkingTimeByUser({ id });
+  }
+
+  @Get('get-all')
+  async getAllUser(): Promise<UserResponseDto[]> {
+    const users = await this.userService.users({});
+
+    if (!users.length) {
+      throw new NotFoundException(`None users found`);
+    }
+
+    return users.map((user: User) => new UserResponseDto(user));
+  }
+
+  @Get('contains/:searchString')
+  async getUser(
+    @Param('searchString') searchString: string,
+  ): Promise<UserModel[]> {
+    const users = await this.userService.users({
+      where: {
+        OR: [
+          { name: { contains: searchString } },
+          { email: { contains: searchString } },
+        ],
+      },
+    });
+
+    if (!users.length) {
+      throw new NotFoundException(`User ${searchString} not found`);
+    }
+
+    return users;
   }
 }
