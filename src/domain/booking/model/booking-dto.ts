b979a484a750a65cdb434f6758059c2272f9723d
Dikendev <diken.dev@gmail.com>
2024-03-04 23:00:30 -0300

    feat add booking dto

---
+++ src/domain/booking/model/booking-dto.ts
@@ -0,0 +1,30 @@
+import { Booking as BookingModel } from '@prisma/client';
+import { IsDateString, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class BookingDTO
+  implements Omit<BookingModel, 'id' | 'createdAt' | 'updatedAt'>
+{
+  @IsNumber()
+  @IsNotEmpty()
+  total: number;
+
+  @IsDateString()
+  @IsNotEmpty()
+  startAt: Date;
+
+  @IsDateString()
+  @IsNotEmpty()
+  finishAt: Date;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  clientId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  serviceId: number;
+}
