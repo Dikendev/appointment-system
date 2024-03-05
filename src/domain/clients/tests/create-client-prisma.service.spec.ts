8e111592b31e6495045282dc2223da49c8c64f48
Dikendev <diken.dev@gmail.com>
2024-03-04 22:50:14 -0300

    update change prisma service path

--- src/domain/clients/tests/create-client-prisma.service.spec.ts
+++ src/domain/clients/tests/create-client-prisma.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaService } from '../../../../prisma/prisma.service';
+import { PrismaService } from '../../../external/prisma/prisma.service';
 import { ClientPrismaService } from '../repository/client-prisma.service';
 import { clientResponseMock, prismaMock } from './create-client-prisma-mock';
 import { NotFoundException } from '@nestjs/common';
