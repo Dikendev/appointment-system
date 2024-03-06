eff91e99975d078f3714a9ef4481a527837c33e6
Dikendev <diken.dev@gmail.com>
2024-03-05 23:46:43 -0300

    feat update procedure path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
-import { ServiceService } from './domain/services/service.service';
+import { ProcedureService } from './domain/procedures/procedure.service';
 import { BookingService } from './domain/booking/booking.service';
-import { ServiceController } from './domain/services/service.controller';
+import { ServiceController } from './domain/procedures/procedure.controller';
 import { BookingController } from './domain/booking/booking.controller';
 import { UsersModule } from './domain/users/users.module';
 import { ClientModule } from './domain/clients/client.module';
@@ -23,6 +23,6 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule,
   ],
   controllers: [AppController, ServiceController, BookingController],
-  providers: [AppService, ServiceService, BookingService],
+  providers: [AppService, ProcedureService, BookingService],
 })
 export class AppModule {}
