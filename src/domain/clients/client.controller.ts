6daf5319fdd525598a2cae659807c20536de3b26
Dikendev <diken.dev@gmail.com>
2024-03-09 13:07:23 -0300

    feat add  delete method client by id

--- src/domain/clients/client.controller.ts
+++ src/domain/clients/client.controller.ts
@@ -1,18 +1,44 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Inject,
   Param,
   Post,
 } from '@nestjs/common';
 import { ClientResponse, CreateClientDTO } from './model/client.model';
 import { ClientRepository } from './repository/client.repository';
+import {
+  Logger,
+  LoggerKey,
+} from '../../external/logger/domain/logger.repository';
 
 @Controller('clients')
 export class ClientController {
-  constructor(private readonly clientRepository: ClientRepository) {}
+  constructor(
+    private readonly clientRepository: ClientRepository,
+    @Inject(LoggerKey) private logger: Logger,
+  ) {}
+
+  @Get()
+  async clients(): Promise<ClientResponse[]> {
+    this.logger.info('Get all clients!', {
+      props: {
+        foo: 'bar',
+        baz: 'qux',
+      },
+    });
+    const clients = await this.clientRepository.clients();
+    return clients;
+  }
+
+  @Get(':id')
+  async clientById(@Param('id') id: number): Promise<ClientResponse> {
+    return this.clientRepository.client(id);
+  }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
@@ -21,14 +47,8 @@ export class ClientController {
     return clientResponse;
   }
 
-  @Get()
-  async getAllClients(): Promise<ClientResponse[]> {
-    const clients = await this.clientRepository.clients();
-    return clients;
-  }
-
-  @Get(':id')
-  async getClient(@Param('id') id: number): Promise<ClientResponse> {
-    return this.clientRepository.client(id);
+  @Delete('client/:id')
+  async deleteClient(@Param('id') id: number) {
+    return this.clientRepository.deleteClient(id);
   }
 }
