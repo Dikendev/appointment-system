615fc3dd38322b1e5dc9a496dcb9685a219601fb
Dikendev <diken.dev@gmail.com>
2024-03-11 22:26:35 -0300

    feat add procedure module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
-import { ProcedureService } from './domain/procedures/procedure.service';
 import { BookingService } from './domain/booking/booking.service';
-import { ServiceController } from './domain/procedures/procedure.controller';
 import { BookingController } from './domain/booking/booking.controller';
 import { UsersModule } from './domain/users/users.module';
 import { ClientModule } from './domain/clients/client.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProcedureModule } from './domain/procedures/procedure.module';
 
 @Module({
   imports: [
-    UsersModule,
+    ConfigModule,
     CacheModule.register({
       isGlobal: true,
       store: 'memory',
       max: 10,
       ttl: 600,
     }),
+    UsersModule,
     ClientModule,
-    ConfigModule,
+    ProcedureModule,
   ],
-  controllers: [AppController, ServiceController, BookingController],
-  providers: [AppService, ProcedureService, BookingService],
+  controllers: [AppController, BookingController],
+  providers: [AppService, BookingService],
 })
 export class AppModule {}
