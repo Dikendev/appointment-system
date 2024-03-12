100c171cc03448cfc51bf19003745f5711e528cd
Dikendev <diken.dev@gmail.com>
2024-03-11 21:13:45 -0300

    feat add delete client method

--- src/domain/clients/repository/client.repository.ts
+++ src/domain/clients/repository/client.repository.ts
@@ -1,7 +1,9 @@
-import { ClientResponse, CreateClientDTO } from '../model/client.model';
+import { CreateClientDTO } from '../models/client-model.dto';
+import { ClientResponse } from '../models/client-model-response';
 
 export abstract class ClientRepository {
   abstract client: (id: number) => Promise<ClientResponse>;
   abstract clients: () => Promise<ClientResponse[]>;
   abstract createClient: (data: CreateClientDTO) => Promise<ClientResponse>;
+  abstract deleteClient: (id: number) => Promise<ClientResponse>;
 }
