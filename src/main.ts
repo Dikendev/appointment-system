26375daab2929e98292e3eacc61bfc7049ff318a
Dikendev <diken.dev@gmail.com>
2024-07-08 23:29:28 -0300

    change the main function to IIFE

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerServiceAdapter } from './external/logger/infrastructure/logger-service-adapter';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+(async () => {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(LoggerServiceAdapter));
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
   app.enableCors();
   await app.listen(3000);
-}
-bootstrap();
+})();
