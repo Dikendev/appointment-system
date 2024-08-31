ab3b118a9e50ae47803a40c5ade3f883ef2080d9
Dikendev <diken.dev@gmail.com>
2024-08-30 23:38:33 -0300

    add prisma exception

---
+++ src/infrastructure/prisma-exception/prisma-exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+
+@Catch(PrismaClientKnownRequestError, PrismaClientValidationError)
+export class PrismaExceptionFilter
+  extends BaseExceptionFilter
+  implements ExceptionFilter
+{
+  catch(exception: Error, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse();
+    const message = exception.message;
+
+    switch (exception.constructor) {
+      case PrismaClientKnownRequestError:
+      case PrismaClientValidationError:
+        response.status(HttpStatus.BAD_REQUEST).json({
+          message: message,
+        });
+        break;
+      default:
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
