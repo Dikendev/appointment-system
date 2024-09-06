ccc4feea1ac98a2b193f34f85bf899ec6feb414b
Dikendev <diken.dev@gmail.com>
2024-09-05 23:28:58 -0300

    chore: Move Prisma module and service to infrastructure/database folder

--- src/infrastructure/database/prisma/prisma.service.ts
+++ src/infrastructure/database/prisma/prisma.service.ts
@@ -1,16 +1,27 @@
-import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+@Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
-    console.log('onModuleInit PrismaService');
     await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
   }
 }
