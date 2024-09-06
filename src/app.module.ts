dcd94e48998c148edea75ba018fc64d76bff7b6e
Dikendev <diken.dev@gmail.com>
2024-09-05 23:33:33 -0300

    Refactor app module imports for better organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CacheModule } from '@nestjs/cache-manager';
-import { BookingService } from './domain/booking/booking.service';
-import { BookingController } from './domain/booking/booking.controller';
-import { UsersModule } from './domain/users/users.module';
-import { ClientInfrastructureModule } from './domain/clients/client.infrastructure.module';
-import { ConfigModule } from '@nestjs/config';
-import { ProcedureModule } from './domain/procedures/procedure.module';
+import { LoggerModule } from './external/logger/infrastructure/logger.module';
+import { ContextModule } from './context/infrastructure/context.module';
+import { ConfigModule } from './config/infrastructure/nestjs/config.module';
+import {
+  BookingModule,
+  ClientModule,
+  ProcedureModule,
+  ProfileModule,
+  UserModule,
+  WorkingTimeModule,
+} from './domain/modules';
+import { PrismaModule } from './infrastructure/database/prisma/prisma.module';
+import { AuthModule } from './domain/auth/auth.module';
+import { RepositoryModule } from './domain/repositories/repository.module';
 
 @Module({
   imports: [
+    PrismaModule,
+    AuthModule,
+    LoggerModule,
+    ContextModule,
     ConfigModule,
-    CacheModule.register({
-      isGlobal: true,
-      store: 'memory',
-      max: 10,
-      ttl: 600,
-    }),
-    ClientInfrastructureModule,
-    UsersModule,
+    UserModule,
     ProcedureModule,
+    ProfileModule,
+    BookingModule,
+    WorkingTimeModule,
+    ClientModule,
+    RepositoryModule,
   ],
-  controllers: [AppController, BookingController],
-  providers: [AppService, BookingService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
