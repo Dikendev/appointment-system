d776f8719e816c3b0e17824c1366108d6d56284a
Dikendev <diken.dev@gmail.com>
2024-09-05 23:27:41 -0300

    feat: Update exception filter to use express Request and Response types

--- src/infrastructure/filter/exception.filter.ts
+++ src/infrastructure/filter/exception.filter.ts
@@ -1,12 +1,13 @@
 import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class ExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const context = host.switchToHttp();
-    const response = context.getResponse();
-    const request = context.getRequest();
-    const status = exception.getResponse();
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+    const status = exception.getStatus();
     const message = exception.message;
 
     response.status(status).json({
