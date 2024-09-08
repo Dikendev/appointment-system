2afd609aa583c644ef85a788d3a03cfb6a6edb1b
Dikendev <diken.dev@gmail.com>
2024-09-07 23:44:31 -0300

    refactor: Add cookie-parser middleware to global config

--- src/config/infrastructure/nestjs/global.config.ts
+++ src/config/infrastructure/nestjs/global.config.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { LoggerServiceAdapter } from '../../../external/logger/infrastructure/logger-service-adapter';
+import * as cookieParser from 'cookie-parser';
 
 export function globalConfig(app: INestApplication): void {
   app.useLogger(app.get(LoggerServiceAdapter));
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   app.enableShutdownHooks();
   app.enableCors();
 }
