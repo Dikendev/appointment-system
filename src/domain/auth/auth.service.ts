d34bcba0cc997ba5b74af834a690b08236647a25
Dikendev <diken.dev@gmail.com>
2024-09-08 23:03:32 -0300

    Refactor AuthService to more SOLID principals

--- src/domain/auth/auth.service.ts
+++ src/domain/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import {
   BadRequestException,
+  HttpException,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -8,9 +10,15 @@ import { Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { LoginDto } from './dto/login-dto';
 import * as bcrypt from 'bcrypt';
-import { RegisterDto, UserDto } from '../entities/dtos/user.dto';
+import { RegisterDto } from '../entities/dtos/user.dto';
 import { User } from '../entities/models';
 import { IUserRepository } from '../repositories/user/user-repository.interface';
+import {
+  Logger,
+  LoggerKey,
+} from '../../external/logger/domain/logger.repository';
+import { UserFactory } from '../use-cases/user/user-factory.service';
+import { hashPassword } from './hash-password';
 
 export interface Logout {
   message: string;
@@ -20,84 +28,90 @@ export interface AuthResponse {
   user: User;
 }
 
+export interface Payload extends JwtSignIn {
+  iat: number;
+  exp: number;
+}
+
+interface JwtSignIn {
+  email: string;
+  sub: string;
+}
+
+const JwtTokenSecret = {
+  ACCESS_JWT_SECRET: 'ACCESS_JWT_SECRET',
+  REFRESH_JWT_SECRET: 'REFRESH_JWT_SECRET',
+} as const;
+
+type JwtTokenSecret = keyof typeof JwtTokenSecret;
+
 @Injectable()
 export class AuthService {
   constructor(
+    @Inject(LoggerKey) private readonly logger: Logger,
     private readonly jwtService: JwtService,
     private readonly userRepository: IUserRepository,
     private readonly configService: ConfigService,
   ) {}
 
-  async refreshToken(request: Request, response: Response): Promise<string> {
+  async refreshToken(request: Request, response: Response): Promise<User> {
+    this.logger.info('Refreshing token');
+
     const refreshToken = request.cookies['refresh_token'];
 
     if (!refreshToken) {
-      throw new UnauthorizedException('Refresh token not found');
+      throw new UnauthorizedException('Need to be logged in');
     }
 
-    let payload: unknown;
+    let payload: Payload;
 
     try {
-      payload = this.jwtService.verify(refreshToken, {
+      payload = this.jwtService.verify<Payload>(refreshToken, {
         secret: this.configService.get('REFRESH_JWT_SECRET'),
       });
 
-      console.log('Payload', payload);
-
-      const user = await this.userRepository.findByEmail(payload['email']);
+      const payloadEmail = payload.email;
+      const user = await this.userRepository.findByEmail(payloadEmail);
 
       if (!user) throw new UnauthorizedException('User not found');
 
-      const expiredIn = new Date().getDate() + 7;
-      const expiration = Math.floor(expiredIn / 1000);
+      const accessToken = this.accessTokenSignIn({
+        email: payloadEmail,
+        sub: user.id,
+      });
 
-      if (payload instanceof Object) {
-        const accessToken = this.jwtService.sign(
-          { ...payload, exp: expiration },
-          { secret: this.configService.get('ACCESS_JWT_SECRET') },
-        );
-
-        response.cookie('access_token', accessToken, { httpOnly: true });
-
-        return accessToken;
-      }
-      throw new UnauthorizedException('Invalid payload');
+      response.cookie('access_token', accessToken, { httpOnly: true });
+      return user;
     } catch (error) {
+      console.log('error', error);
       throw new UnauthorizedException('Invalid or expired refresh token');
     }
   }
 
-  async issueToken(
-    userResponse: User,
-    response: Response,
-  ): Promise<AuthResponse> {
+  async issueToken(userResponse: User, response: Response): Promise<User> {
     const payload = { email: userResponse.profile.email, sub: userResponse.id };
 
-    const token = this.jwtService.sign(
-      { ...payload },
-      {
-        secret: this.configService.get('ACCESS_JWT_SECRET'),
-        expiresIn: '2d',
-      },
-    );
+    const accessToken = this.accessTokenSignIn({
+      ...payload,
+    });
 
     const refreshToken = this.jwtService.sign(payload, {
       secret: this.configService.get('REFRESH_JWT_SECRET'),
       expiresIn: '7d',
     });
 
-    response.cookie('access_token', token, { httpOnly: true });
+    response.cookie('access_token', accessToken, { httpOnly: true });
     response.cookie('refresh_token', refreshToken, { httpOnly: true });
 
-    return {
-      user: userResponse,
-    };
+    return userResponse;
   }
 
-  async validateUser(body: LoginDto) {
+  async validateUser(body: LoginDto): Promise<User> {
     const user = await this.userRepository.findByEmail(body.email);
 
-    if (!user) throw new Error('User not found');
+    if (!user) {
+      throw new HttpException('Email or Password invalid, try again', 404);
+    }
 
     const hashedPassword = user.password;
     const isValidPassword = await this.comparePasswords(
@@ -106,41 +120,25 @@ export class AuthService {
     );
 
     if (!isValidPassword) throw new Error('Invalid password');
-    return user;
+
+    return UserFactory.omitPassword(user);
   }
 
-  async comparePasswords(
-    hashedPassword: string,
-    provided: string,
-  ): Promise<boolean> {
-    return bcrypt.compare(provided, hashedPassword);
-  }
-
-  async register(body: UserDto, response: Response): Promise<AuthResponse> {
-    console.log('Registering user', body);
-
-    const existingUser = await this.userRepository.findByEmail(
-      body.profile.email,
-    );
-
-    if (existingUser) throw new Error('User already exists');
-
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(body.password, salt);
+  async register(body: RegisterDto, response: Response): Promise<User> {
+    const hashedPassword = await hashPassword(body.password);
 
     const user = await this.userRepository.create({
-      ...body,
+      name: body.name,
+      profile: { email: body.email },
       password: hashedPassword,
     });
 
-    console.log('User created', user);
-    return this.issueToken(user, response);
+    const token = await this.issueToken(user, response);
+    return token;
   }
 
   async login(loginDto: LoginDto, response: Response) {
     const user = await this.validateUser(loginDto);
-
     if (!user) throw new Error('Invalid credentials');
     return this.issueToken(user, response);
   }
@@ -148,6 +146,17 @@ export class AuthService {
   async logout(response: Response): Promise<Logout> {
     this.clearCookies(['access_token', 'refresh_token'], response);
     return { message: 'Logged out' };
+  }
+
+  async registerUser(body: RegisterDto, response: Response): Promise<User> {
+    const existingUser = await this.userRepository.findByEmail(body.email);
+    if (existingUser) throw new BadRequestException('User already exists');
+
+    if (body.password !== body.confirmPassword) {
+      throw new BadRequestException('Passwords do not match');
+    }
+
+    return this.register(body, response);
   }
 
   async clearCookies(
@@ -159,19 +168,61 @@ export class AuthService {
     });
   }
 
-  async registerUser(
-    registerDto: RegisterDto,
-    response: Response,
-  ): Promise<User> {
-    if (registerDto.password !== registerDto.confirmPassword) {
-      throw new BadRequestException({
-        confirmPassword: 'Password and confirm password must be the same',
-      });
+  async comparePasswords(
+    hashedPassword: string,
+    provided: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(provided, hashedPassword);
+  }
+
+  validateAccessToken(accessToken: string): Payload {
+    try {
+      const secret = this.configService.get('ACCESS_JWT_SECRET');
+      return this.validateToken(accessToken, secret);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid access token');
     }
+  }
 
-    const { user } = await this.register(registerDto, response);
-    console.log('REGISTERING A USER', user);
+  validateRefreshToken(refreshToken: string): Payload {
+    try {
+      const secret = this.configService.get('REFRESH_JWT_SECRET');
+      return this.validateToken(refreshToken, secret);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+  }
 
-    return user;
+  validateToken(token: string, secretKey: string): Payload {
+    const tokenData = this.jwtService.verify<Payload>(token, {
+      secret: secretKey,
+    });
+    return tokenData;
+  }
+
+  accessTokenSignIn(payload: JwtSignIn): string {
+    const expiresIn = this.configService.get<string>('ACCESS_TOKEN_EXPIRATION');
+    return this.jwtSignIn(payload, 'ACCESS_JWT_SECRET', expiresIn);
+  }
+
+  refreshTokenSignIn(payload: JwtSignIn): string {
+    const expiresIn = this.configService.get<string>(
+      'REFRESH_TOKEN_EXPIRATION',
+    );
+    return this.jwtSignIn(payload, 'ACCESS_JWT_SECRET', expiresIn);
+  }
+
+  private jwtSignIn(
+    payload: JwtSignIn,
+    jwtTokenSecret: JwtTokenSecret,
+    expiresIn: string,
+  ): string {
+    return this.jwtService.sign(
+      { ...payload },
+      {
+        secret: this.configService.get<string>(jwtTokenSecret),
+        expiresIn: expiresIn,
+      },
+    );
   }
 }
