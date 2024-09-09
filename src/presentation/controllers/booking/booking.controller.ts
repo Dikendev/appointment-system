070f7abb5ba459a814afabf94b1fe9726084bee5
Dikendev <diken.dev@gmail.com>
2024-09-08 23:08:33 -0300

    Refactor BookingController to use string IDs instead of numbers and add authentication endpoints

--- src/presentation/controllers/booking/booking.controller.ts
+++ src/presentation/controllers/booking/booking.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { BookingUseCase } from '../../../domain/use-cases/booking/booking.use-case';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { BookingDto } from '../../../domain/entities/dtos';
-import { Booking } from '../../../domain/entities/models';
+import { Booking, User } from '../../../domain/entities/models';
+import { JwtAuthGuard } from '../../../domain/auth/jwt-strategy/jwt-auth.guard';
+import { UserLogged } from '../../user.decorator';
+import { BookingUseCase } from '../../../domain/use-cases/booking/booking.use-case';
+import { TotalEarnedToday } from '../../../domain/use-cases/booking/interfaces/total-earned-today.interface';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingUseCase: BookingUseCase) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() body: BookingDto): Promise<Booking> {
     return this.bookingUseCase.create(body);
@@ -18,9 +22,23 @@ export class BookingController {
   //   return bookings;
   // }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async findAllByUserId(@UserLogged() user: User): Promise<Booking[]> {
+    console.log('user decorator', user);
+    return this.bookingUseCase.findAllBookingsByUserId(user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(): Promise<any> {
     return await this.bookingUseCase.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('earned-today/:id')
+  async earnedTodayByUser(@Param('id') id: string): Promise<TotalEarnedToday> {
+    return await this.bookingUseCase.earnedToday(id);
   }
 
   // @Get('by-day')
