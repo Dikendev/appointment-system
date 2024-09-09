60f5f56cda4589189296799f72a2e933341edf18
Dikendev <diken.dev@gmail.com>
2024-09-08 23:04:22 -0300

    Refactor User class to use public fields in constructor

--- src/domain/entities/models/user.ts
+++ src/domain/entities/models/user.ts
@@ -3,36 +3,14 @@ import { Booking } from './booking';
 import { Profile } from './profile';
 
 export class User {
-  id: string;
-  name: string;
-  profile: Profile;
-  password: string;
-  createdAt: Date;
-  updatedAt: Date;
-  workingTimes?: WorkingTime[];
-  bookings?: Booking[];
-
-  constructor(user: Partial<User>) {
-    this.id = user.id;
-    this.name = user.name;
-    this.profile = user.profile;
-    this.createdAt = user.createdAt;
-    this.updatedAt = user.updatedAt;
-    this.workingTimes = user?.workingTimes;
-    this.bookings = user?.bookings;
-  }
-}
-
-export class UserFactory {
-  static userResponse(data: User): User {
-    return new User({
-      id: data.id,
-      name: data.name,
-      profile: data.profile,
-      createdAt: data.createdAt,
-      updatedAt: data.updatedAt,
-      workingTimes: data.workingTimes,
-      bookings: data.bookings,
-    });
-  }
+  constructor(
+    public id: string,
+    public name: string,
+    public profile: Profile,
+    public createdAt: Date,
+    public updatedAt: Date,
+    public workingTimes?: WorkingTime[],
+    public bookings?: Booking[],
+    public password?: string,
+  ) {}
 }
