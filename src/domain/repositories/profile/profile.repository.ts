06248f02c8817ea7a5d532fe51a6b02f3a2c73bf
Dikendev <diken.dev@gmail.com>
2024-09-08 23:11:29 -0300

    Refactor ProfileRepository to use string IDs instead of numbers and add update methods for user and client profiles

--- src/domain/repositories/profile/profile.repository.ts
+++ src/domain/repositories/profile/profile.repository.ts
@@ -2,16 +2,42 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../infrastructure/database/prisma/prisma.service';
 import { Profile } from '../../entities/models';
 import { IProfileRepository } from './profile-repository.interface';
+import { ProfileUpdateDto } from '../../entities/dtos';
 
 @Injectable()
 export class ProfileRepository implements IProfileRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findOne(userId: number): Promise<Profile> {
-    return this.prismaService.profile.findUnique({ where: { userId: userId } });
+  async me(email: string): Promise<Profile> {
+    return this.prismaService.profile.findUnique({
+      where: {
+        email: email,
+      },
+    });
   }
 
-  async create(): Promise<Profile> {
-    throw new Error('Method not implemented.');
+  async updateUser(id: string, body: ProfileUpdateDto): Promise<Profile> {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: id },
+      include: { profile: true },
+    });
+
+    return this.prismaService.profile.update({
+      where: {
+        id: String(user.profile.id),
+      },
+      data: { ...body },
+    });
+  }
+
+  async updateClient(id: string, body: ProfileUpdateDto): Promise<Profile> {
+    return this.prismaService.profile.update({
+      where: {
+        clientId: id,
+      },
+      data: {
+        ...body,
+      },
+    });
   }
 }
