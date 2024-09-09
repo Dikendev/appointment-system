44261e923fdee90f5298fc85cb5594d438045859
Dikendev <diken.dev@gmail.com>
2024-09-08 23:09:46 -0300

    Refactor ProfileController to use authentication and add findProfile and updateUserProfile methods

--- src/presentation/controllers/profile/profile.controller.ts
+++ src/presentation/controllers/profile/profile.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../../../domain/auth/jwt-strategy/jwt-auth.guard';
+import { Profile } from '../../../domain/entities/models';
+import { Request } from 'express';
+import { ProfileUseCase } from '../../../domain/use-cases/profile/profile.use-case';
+import { ProfileUpdateDto } from '../../../domain/entities/dtos';
 
 @Controller('profile')
 export class ProfileController {
-  constructor() {}
+  constructor(private readonly profileUseCase: ProfileUseCase) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async findProfile(@Req() request: Request): Promise<Profile> {
+    return this.profileUseCase.me(request);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  updateUserProfile(
+    @Req() request: Request,
+    @Body() body: ProfileUpdateDto,
+  ): Promise<Profile> {
+    return this.profileUseCase.updateUser(request, body);
+  }
 }
