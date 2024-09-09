be187c94b534a78deb82177165371202001317ef
Dikendev <diken.dev@gmail.com>
2024-09-08 23:06:15 -0300

    Refactor UserController to use string IDs instead of numbers and add authentication endpoints

--- src/presentation/controllers/user/user.controller.ts
+++ src/presentation/controllers/user/user.controller.ts
@@ -1,31 +1,55 @@
-import { Body, Controller, Delete, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { RegisterDto } from '../../../domain/entities/dtos/user.dto';
 import { UserUseCase } from '../../../domain/use-cases/user/user.use-case';
 import { User } from '../../../domain/entities/models/user';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import {
+  JwtAuthGuard,
+  Public,
+} from '../../../domain/auth/jwt-strategy/jwt-auth.guard';
+import { LoginDto } from '../../../domain/auth/dto/login-dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userUseCase: UserUseCase) {}
 
+  @Public()
   @Post('register')
   async register(@Body() registerDto: RegisterDto, @Res() response: Response) {
-    return this.userUseCase.register(registerDto, response);
+    const user = await this.userUseCase.register(registerDto, response);
+    response.status(201).send(user);
   }
 
+  @Public()
+  @Post('login')
+  async login(@Body() body: LoginDto, @Res() response: Response) {
+    const user = await this.userUseCase.login(body, response);
+    response.status(201).send(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('logout')
+  async logout(@Req() request: Request, @Res() response: Response) {
+    const user = await this.userUseCase.logout(request, response);
+    response.status(201).send(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get()
   async list(): Promise<User[]> {
     return this.userUseCase.users();
   }
 
-  @Post()
-  async createUser(
-    @Body() registerDto: RegisterDto,
-    @Res() response: Response,
-  ): Promise<User> {
-    return this.userUseCase.register(registerDto, response);
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Delete('wipe')
   async wipe() {
     return this.userUseCase.wipe();
