e86a98710e52d9882ee0a5c29fb6552a79e4eeab
Dikendev <diken.dev@gmail.com>
2024-09-08 23:10:06 -0300

    Refactor ProfileUseCase to use authentication and add me and updateUser methods

--- src/domain/use-cases/profile/profile.use-case.ts
+++ src/domain/use-cases/profile/profile.use-case.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { Profile } from '../../entities/models';
 import { IProfileRepository } from '../../repositories/profile/profile-repository.interface';
+import { Request } from 'express';
+import { ProfileUpdateDto } from '../../entities/dtos';
 
 @Injectable()
 export class ProfileUseCase {
   constructor(private readonly profileRepository: IProfileRepository) {}
 
-  async findOne(userId: number): Promise<Profile> {
-    return this.profileRepository.findOne(userId);
+  async me(request: Request): Promise<Profile> {
+    const payload = request.user;
+    return this.profileRepository.me(payload.profile.email);
   }
 
-  async create(): Promise<Profile> {
-    return this.profileRepository.create();
+  async updateUser(request: Request, body: ProfileUpdateDto): Promise<Profile> {
+    const payload = request.user;
+    return this.profileRepository.updateUser(payload.id, body);
+  }
+
+  async updateClient(id: string, body: ProfileUpdateDto): Promise<Profile> {
+    return this.profileRepository.updateClient(id, body);
   }
 }
