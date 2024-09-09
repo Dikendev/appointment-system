f62042e68dae50f46e4a9e1ded1de9c338ecfa6d
Dikendev <diken.dev@gmail.com>
2024-09-08 23:09:37 -0300

    Refactor ClientController to use string IDs instead of numbers and add authentication endpoints

--- src/presentation/controllers/client/client.controller.ts
+++ src/presentation/controllers/client/client.controller.ts
@@ -3,38 +3,41 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ClientUseCase } from '../../../domain/use-cases/client/client.use-case';
 import { Client } from '../../../domain/entities/models';
 import { ClientDto } from '../../../domain/entities/dtos';
+import { JwtAuthGuard } from '../../../domain/auth/jwt-strategy/jwt-auth.guard';
 
-@Controller('clients')
+@Controller('client')
 export class ClientController {
   constructor(private readonly clientUseCase: ClientUseCase) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async clientById(@Param('id') id: string): Promise<Client> {
-    return this.clientUseCase.findById(Number(id));
+    return this.clientUseCase.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async clients(): Promise<Client[]> {
     return this.clientUseCase.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  @HttpCode(HttpStatus.CREATED)
   async create(@Body() data: ClientDto): Promise<Client> {
     const clientResponse = await this.clientUseCase.create(data);
     return clientResponse;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  async deleteClient(@Param('id') id: number): Promise<Client> {
+  async deleteClient(@Param('id') id: string): Promise<Client> {
     return this.clientUseCase.delete(id);
   }
 }
